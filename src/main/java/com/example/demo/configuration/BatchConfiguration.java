af1ca0b8e70dc86399a384660c5a45c0f6e4838d
Tomoya Sasaki <tomoya.metropolis.part2@gmail.com>
2026-06-21 16:54:06 +0900

    Update

--- src/main/java/com/example/demo/configuration/BatchConfiguration.java
+++ src/main/java/com/example/demo/configuration/BatchConfiguration.java
@@ -22,13 +22,10 @@ import org.springframework.batch.infrastructure.item.support.CompositeItemProces
 import org.springframework.batch.infrastructure.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.infrastructure.item.validator.ValidationException;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.example.demo.domain.Member;
@@ -38,32 +35,8 @@ import com.example.demo.listener.ValidationErrorLoggingListener;
 
 @Configuration
 @EnableJdbcJobRepository
+@Import(value = DataSourceConfiguration.class)
 public class BatchConfiguration {
-
-	@Bean("dataSource")
-	@Primary
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean("transactionManager")
-	@Primary
-	public PlatformTransactionManager transactionManager(@Qualifier("dataSource") DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
-	}
-
-	@Bean("businessDataSource")
-	@ConfigurationProperties(prefix = "spring.business.datasource")
-	public DataSource businessDataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean("businessTransactionManaber")
-	public PlatformTransactionManager businessTransactionManager(
-			@Qualifier("businessDataSource") DataSource businessDataSource) {
-		return new DataSourceTransactionManager(businessDataSource);
-	}
 
 	@Bean
 	public FlatFileItemReader<Member> itemReader() {
@@ -103,13 +76,15 @@ public class BatchConfiguration {
 	}
 
 	@Bean
-	public Step memberStep(JobRepository jobRepository, ItemReader<Member> itemReader,
-			ItemProcessor<Member, MemberWithFullName> itemProcessor,
+	public Step memberStep(JobRepository jobRepository,
+			@Qualifier("businessTransactionManager") PlatformTransactionManager businessTransactionManager,
+			ItemReader<Member> itemReader, ItemProcessor<Member, MemberWithFullName> itemProcessor,
 			ItemProcessListener<Member, MemberWithFullName> itemProcessListener,
 			ItemWriter<MemberWithFullName> itemWriter, StepExecutionListener stepExecutionListener) {
-		return new StepBuilder("memberStep", jobRepository).<Member, MemberWithFullName>chunk(2).reader(itemReader)
-				.processor(itemProcessor).listener(itemProcessListener).writer(itemWriter)
-				.listener(stepExecutionListener).faultTolerant().retryLimit(1).skip(ValidationException.class).build();
+		return new StepBuilder("memberStep", jobRepository).<Member, MemberWithFullName>chunk(2)
+				.transactionManager(businessTransactionManager).reader(itemReader).processor(itemProcessor)
+				.listener(itemProcessListener).writer(itemWriter).listener(stepExecutionListener).faultTolerant()
+				.retryLimit(1).skip(ValidationException.class).build();
 	}
 
 	@Bean
